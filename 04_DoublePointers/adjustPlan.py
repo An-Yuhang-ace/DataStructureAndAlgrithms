e1e32affa65a8d7a3fa573886e754442b65f5e70
yuhang.an <695100218@qq.com>
2023-09-22 17:51:04 +0800

    update double pointers examples

--- 04_DoublePointers/adjustPlan.py
+++ 04_DoublePointers/adjustPlan.py
@@ -1,1 +1,18 @@
 # https://leetcode.cn/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/
+
+def trainingPlan(a):
+    j = 0
+    for i in range(len(a)):
+        if a[i] % 2 == 1:
+            swap(a, i, j)
+            j += 1
+    return a  
+
+def swap(a, i, j):
+    temp = a[j]
+    a[j] = a[i]
+    a[i] = temp
+
+if __name__ == "__main__":
+    actions = [1,2,3,4,5]
+    print(trainingPlan(actions))
--- 04_DoublePointers/cycleLinkedList.py
+++ 04_DoublePointers/cycleLinkedList.py
@@ -1,1 +1,30 @@
 # https://leetcode.cn/problems/linked-list-cycle-ii/
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head):
+        if head == None:
+            return None
+        slow, fast = head, head.next
+        while slow != fast:
+            if slow != None:
+                slow = slow.next
+            if fast != None:
+                if fast.next != None:
+                    fast = fast.next.next
+                else:
+                    fast = fast.next    
+        if slow == None:
+            return None
+        else:
+            slow = slow.next
+            fast = head
+            while slow != fast:
+                slow = slow.next
+                fast = fast.next
+            return slow
